8564572764e3e7ff2d25b1e2c157ecedf1134e96
Gerrit Lane <lane.gerrit@gmail.com>
2022-08-13 18:16:54 -0700

    Add 3 instruments, keypress listener, color/font

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Web based sound effects board
 
 All images generated with AI using DALL·E by OpenAI
+Audio license CC0 V1.0 by rubberduck on Open Game Art
--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
 const audioFileList = [
 	'bell_01',
 	'door_close_01',
+	'glass_04',
+	'metal_05',
+	'spring_02'
 ];
 
+// Map audio to key presses. Can be WASD123 etc
+let audioKeyMap = {
+	'1': 'bell_01',
+	'2': 'door_close_01',
+	'3': 'glass_04',
+	'4': 'metal_05',
+	'5': 'spring_02'
+}
+
+// Add audio on-click to instrument divs
 for(let i = 0; i < audioFileList.length; i++) {
-	document.querySelectorAll('.instrument__img')[i].addEventListener('click', function(){
+	document.querySelectorAll('div .instrument')[i].addEventListener('click', function(){
 		playAudio(`${audioFileList[i]}`);
 	});
 }
+
+// Add audio on keydown to body mapped to the listed key
+document.querySelector('body').addEventListener('keydown', function(e) {
+		if (e.key in audioKeyMap) {
+			playAudio(`${audioKeyMap[e.key]}`);
+		}
+});
 
 function playAudio (audioFile) {
 	let audio = new Audio(`../audio/${audioFile}.ogg`);
